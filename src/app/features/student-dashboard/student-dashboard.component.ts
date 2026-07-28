c8dbfaae38a265bf91fe1e6c0abff79661e46f6e
Abdulfeta-Sani <abdulfetasani128@gmail.com>
2026-07-28 11:04:37 +0300

    feat(course-card): implement reusable course card with input and output communication

--- src/app/features/student-dashboard/student-dashboard.component.ts
+++ src/app/features/student-dashboard/student-dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component, signal, computed } from '@angular/core';
+import { CourseCardComponent } from '../../ui/course-card/course-card.component';
+import { Course } from '../../models/course.model';
 
 @Component({
   selector: 'app-student-dashboard',
   standalone: true,
+  imports: [CourseCardComponent],
   templateUrl: './student-dashboard.component.html',
   styleUrl: './student-dashboard.component.scss',
 })
@@ -17,5 +20,19 @@ export class StudentDashboardComponent {
 
   registerForClass() {
     this.earnedCredits.update((c) => c + 3);
+  }
+
+  selectedCourse = signal<Course | null>(null);
+  // A sample course to display (we will switch to an array in Excercise3)
+  sampleCourse: Course = {
+    id: 1,
+    title: 'Advanced Java Services',
+    code: 'CSE-101',
+    maxCapacity: 30,
+    enrollmentCount: 12,
+  };
+  handleEnroll(course: Course) {
+    this.selectedCourse.set(course);
+    console.log('Enrollment requested for:', course.title);
   }
 }
