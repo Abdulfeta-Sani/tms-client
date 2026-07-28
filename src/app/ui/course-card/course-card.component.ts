96ede8deb58322142c2c9b04f5347945914a3d85
Abdulfeta-Sani <abdulfetasani128@gmail.com>
2026-07-28 14:00:10 +0300

    feat(routing): implement course catalog and course detail navigation

--- src/app/ui/course-card/course-card.component.ts
+++ src/app/ui/course-card/course-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { Course } from '../../models/course.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'tms-course-card',
   standalone: true,
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.scss',
+  imports: [RouterLink],
 })
 export class CourseCardComponent {
   course = input.required<Course>();
