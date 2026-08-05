a2f02e98f724e4986073dd3c1392ce16aab3ee46
Abdulfeta-Sani <abdulfetasani128@gmail.com>
2026-08-06 01:04:47 +0300

    refactor(enrollments): add Material data grid with reactive signals

--- src/app/features/enrollment-list/enrollment-list.component.ts
+++ src/app/features/enrollment-list/enrollment-list.component.ts
@@ -1,20 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, viewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Enrollment } from '../../models/enrollment.model';
 import { EnrollmentStore } from '../../store/enrollment.store';
 
 @Component({
   selector: 'tms-enrollment-list',
   standalone: true,
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
   templateUrl: './enrollment-list.component.html',
   styleUrl: './enrollment-list.component.scss',
 })
-export class EnrollmentListComponent implements OnInit {
+export class EnrollmentListComponent {
   store = inject(EnrollmentStore);
 
-  ngOnInit() {
-    this.store.loadEnrollments();
-  }
+  displayedColumns = ['studentName', 'courseName', 'status', 'actions'];
 
-  onApprove(id: string) {
-    this.store.approveEnrollment(id);
+  dataSource = new MatTableDataSource<Enrollment>();
+
+  readonly paginator = viewChild.required(MatPaginator);
+  readonly sort = viewChild.required(MatSort);
+
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.store.entities();
+    });
+
+    effect(() => {
+      this.dataSource.paginator = this.paginator();
+      this.dataSource.sort = this.sort();
+    });
+
+    this.store.loadEnrollments();
   }
 }
