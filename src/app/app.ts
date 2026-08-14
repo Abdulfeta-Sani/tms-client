ded0a55daff0dff4b355640c4a841d76728b5464
Abdulfeta-Sani <abdulfetasani128@gmail.com>
2026-08-15 02:23:13 +0300

    feat(M9): activate live sync on app startup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy, OnInit, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { EnrollmentStore } from './store/enrollment.store';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
@@ -7,6 +10,13 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
+  private store = inject(EnrollmentStore);
+
   protected readonly title = signal('tms-client');
+
+  ngOnInit() {
+    this.store.loadEnrollments();
+    this.store.listenForLiveUpdates();
+  }
 }
